816b8089dee74d531491020d638373113ceb65f7
Eric Skoglund <eric@pagefault.se>
2015-10-31 14:09:00 +0100

    removed __main__.py all commands will be handled from the parcel program and the delegated

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ from setuptools import setup
 import parcel
 
 setup(
-    name         = parcel.__title__
-    version      = parcel.__version__
-    author       = parcel.__author__
-    author_email = parcel.__email__
-    license      = parcel.__license__
-    summary      = parcel.__summary__
+    name         = parcel.__title__,
+    version      = parcel.__version__,
+    author       = parcel.__author__,
+    author_email = parcel.__email__,
+    license      = parcel.__license__,
+    summary      = parcel.__summary__,
 
     entry_points = {
         'console_scripts' : [
-            'parcel = parcel.__main__:cli'
+            'parcel = parcel.parcel:cli'
         ]
     }
 )
